6e74bfc63f875792cd44ce369e48083dd6fd03a1
Vinayak Shukla <vinayakshukla0786@gmail.com>
2023-11-04 18:14:43 +0530

    React App initialized

---
+++ vincart-frontend/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from  './customer/components/navigation/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
